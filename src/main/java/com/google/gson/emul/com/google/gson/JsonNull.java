1304989c25665949731e3884114bf679beb121ac
Alex Bertram <alex@bedatadriven.com>
2016-07-12 12:32:28 +0200

    Updated visibility of JsonNull.INSTANCE to match latest version of Gson

--- src/main/java/com/google/gson/emul/com/google/gson/JsonNull.java
+++ src/main/java/com/google/gson/emul/com/google/gson/JsonNull.java
@@ -20,7 +20,8 @@ import com.google.gwt.json.client.JSONNull;
 import com.google.gwt.json.client.JSONValue;
 
 public final class JsonNull extends JsonElement {
-  private static final JsonNull INSTANCE = new JsonNull();
+  
+  public static final JsonNull INSTANCE = new JsonNull();
 
   private JSONNull inner = JSONNull.getInstance();
 
